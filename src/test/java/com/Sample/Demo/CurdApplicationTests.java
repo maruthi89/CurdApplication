8df40641a9bcd742c019ae3311368c12c5297385
maruthi89 <maruthikaran89@gmail.com>
2023-06-19 20:18:58 +0530

    added src folder

---
+++ src/test/java/com/Sample/Demo/CurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sample.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
